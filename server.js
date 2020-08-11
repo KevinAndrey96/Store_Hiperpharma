091664c75ff080aa2a2754c36073650dbca4afec
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-08-11 15:47:57 -0500

    SSL

--- server.js
+++ server.js
@@ -18,6 +18,15 @@
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
+const path = require('path')
+const https = require('https')
+const fs = require('fs')
+
+// Certificate
+const options = {
+  key: fs.readFileSync(path.join(__dirname, './server.key')),
+  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
+}
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
